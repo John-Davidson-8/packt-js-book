8f77e32731daac247ec0df42bebcdcfa0d1ef110
John-Davidson-8 <jodo828@gmail.com>
2023-12-09 15:36:21 +0000

    ch 4 start

---
+++ ch-4 -js-logic-statements/app.js
@@ -0,0 +1,28 @@
+// Chapter 4 Logical Operators
+
+// If and If Else Statements
+// Else if Statements
+
+// Practice Exercise 4.1
+
+const sun = false;
+if (sun) {
+  console.log("I need to take my sun screen");
+}
+if (!sun) {
+  console.log("I don't need my sun screen");
+}
+
+// Practice Exercise 4.2
+
+// let userAge = prompt("What is your age?");
+// userAge = Number(userAge);
+// let message;
+// if (userAge >= 21) {
+//   message = "You are 21 or over and are welcome to come in and drink alcohol";
+// } else if (userAge >= 19) {
+//   message = "You can come in but not drink alcohol";
+// } else {
+//   message = "Sorry, you can't enter as you are under 19";
+// }
+// console.log(message);
