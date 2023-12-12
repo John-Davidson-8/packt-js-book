4ff874414799677cc0545cb867f80b8466900756
John-Davidson-8 <jodo828@gmail.com>
2023-12-12 10:00:56 +0000

    Complete Rock, Paper, Scissors

--- ch-4 -js-logic-statements/app.js
+++ ch-4 -js-logic-statements/app.js
@@ -310,6 +310,8 @@ the result to the user */
 // }
 // console.log(message);
 
+// Rock, Paper Scissors Again!!!
+
 let choice = ["rock", "paper", "scissors"];
 let playerMessage = "you choose";
 let computerMessage = "computer chose";
@@ -336,3 +338,28 @@ if (computerChoice === 1) {
   computerMessage = "computer chose scissors";
 }
 console.log(computerMessage);
+
+// Tie
+if (
+  (playerChoice == choice[0] && computerChoice == 1) ||
+  (playerChoice == choice[1] && computerChoice == 2) ||
+  (playerChoice == choice[2] && computerChoice == 3)
+) {
+  console.log("It is a tie");
+}
+// Player wins
+else if (playerChoice == choice[0] && computerChoice == 3) {
+  console.log("You win! Rock beats Scissors!");
+} else if (playerChoice == choice[1] && computerChoice == 1) {
+  console.log("You win! Paper beats rock!");
+} else if (playerChoice == choice[2] && computerChoice == 2) {
+  console.log("You win scissors beats paper");
+
+  // Computer wins
+} else if (playerChoice == choice[0] && computerChoice == 2) {
+  console.log("You lose! Paper beats rock");
+} else if (playerChoice == choice[1] && computerChoice == 3) {
+  console.log("You lose! Scissors beats paper");
+} else if (playerChoice == choice[2] && computerChoice == 1) {
+  console.log("You lose! Rock beats scissors");
+}
