88bd221f41077a8e2985f3fe0b7f581df87cc5ee
John-Davidson-8 <jodo828@gmail.com>
2024-09-02 21:59:48 +0100

    edit

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -72,3 +72,61 @@ console.log(newList.indexOf("carrots"));
 const anotherArray = [1, 2, 3];
 const nestedArray = [anotherArray, anotherArray, anotherArray];
 console.log(nestedArray);
+
+// Objects
+let dog = {
+  dogName: "Javascript",
+  weight: 2.4,
+  color: "brown",
+  breed: "pug",
+  age: 3,
+  burglarBiter: true,
+};
+// accessing properties 2 ways:
+let dogColor1 = dog["color"];
+console.log(dogColor1);
+
+let dogColor2 = dog.color;
+console.log(dogColor2);
+
+// updating objects
+dog["color"] = "blue";
+dog.weight = 2.3;
+
+console.log(dog);
+
+dog["age"] = "three";
+console.log(dog);
+
+let variable = "age";
+console.log(variable);
+
+// Exercise 3.4
+
+const myCar = {
+  make: "Ford",
+  model: "kuga",
+  color: "blue",
+  doors: 4,
+  manual: false,
+  forSale: false,
+};
+// create a variable that can hold the string value color.
+let propColor = "color"; //this is how to access property and change it to a variable.
+
+/* this variable containing string value (property - color) can be used to reference the property name within myCar */
+myCar[propColor] = "red";
+
+// this is how to change value.
+/* then use the variable within the square bracket notation to assign a new value to forSale property */
+console.log(myCar);
+console.log(myCar.color);
+
+/* then use the variable within the square bracket notation to assign a new value to forSale property */
+
+propColor = "forSale";
+myCar[propColor] = true; // this is how to change value.
+console.log(myCar.make + " " + myCar.model);
+console.log(myCar.forSale);
+
+// Another go at these questions
